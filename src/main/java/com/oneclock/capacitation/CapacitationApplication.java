b7a33746fa3828b0e485e83bf4aa217be48da819
Damian Zamora <desarrollo4@copsis.com>
2021-01-12 11:10:39 -0600

    Initial commit}

---
+++ src/main/java/com/oneclock/capacitation/CapacitationApplication.java
@@ -0,0 +1,13 @@
+package com.oneclock.capacitation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapacitationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapacitationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oneclock/capacitation/CapacitationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oneclock.capacitation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapacitationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
